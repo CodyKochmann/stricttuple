59ecbc58c040d05304b894632a987e1dd8193f4e
Cody Kochmann <kochmanncody@gmail.com>
2017-07-05 15:24:56 -0400

    custom exception types!

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'stricttuple',
   packages = ['stricttuple'], # this must be the same as the name above
-  version = '2017.7.4',
+  version = '2017.7.5',
   install_requires=[],
   description = 'rule based data containers',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/stricttuple',
-  download_url = 'https://github.com/CodyKochmann/stricttuple/tarball/2017.7.4',
+  download_url = 'https://github.com/CodyKochmann/stricttuple/tarball/2017.7.5',
   keywords = ['stricttuple', 'namedtuple', 'tuple', 'design', 'contract', 'assurance', 'strict', 'rule', 'typedtuple'],
   classifiers = [],
 )

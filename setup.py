8b2d27c78f60c2be7359d73feb47465096642d88
Cody Kochmann <kochmanncody@gmail.com>
2017-10-02 14:50:19 -0400

    cleaner tables release

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'stricttuple',
   packages = ['stricttuple'], # this must be the same as the name above
-  version = '2017.9.27',
+  version = '2017.10.2',
   install_requires = ["prettytable"],
   description = 'rule based data containers',
   author = 'Cody Kochmann',
   author_email = 'kochmanncody@gmail.com',
   url = 'https://github.com/CodyKochmann/stricttuple',
-  download_url = 'https://github.com/CodyKochmann/stricttuple/tarball/2017.9.27',
+  download_url = 'https://github.com/CodyKochmann/stricttuple/tarball/2017.10.2',
   keywords = ['stricttuple', 'namedtuple', 'tuple', 'design', 'contract', 'assurance', 'strict', 'rule', 'typedtuple'],
   classifiers = [],
 )
